a34a3e7f3e561688c8a86549a032e4dbcf91e9b5
Joona Piirainen <joona.piirainen@gmail.com>
2020-07-30 14:23:37 +0300

    compose

---
+++ compose.js
@@ -0,0 +1,67 @@
+import R from 'ramda'
+const _ = R
+
+const toUpper = str => str.toUpperCase()
+const concat = _.curry((y, x) => x + y)
+const exclaim = str => str + '!'
+const log = _.curry((tag, x) => (console.log(tag, x), x))
+
+const customCompose = (f, g) => x => f(g(x))
+
+const shout = customCompose(exclaim, toUpper)
+
+console.log(shout('Hello World'))
+
+const lorem = _.compose(concat(' LOL'), log('almost there:'), toUpper, log('next:'), exclaim, log('start:'))
+
+console.log(lorem('Lorem Ipsum'))
+
+
+//ex1
+
+const CARS = [
+    {name: "Ferrari FF", horsepower: 660, dollar_value: 700000, in_stock: true},
+    {name: "Spyker C12 Zagato", horsepower: 650, dollar_value: 648000, in_stock: false},
+    {name: "Jaguar XKR-S", horsepower: 550, dollar_value: 132000, in_stock: false},
+    {name: "Audi R8", horsepower: 525, dollar_value: 114200, in_stock: false},
+    {name: "Aston Martin One-77", horsepower: 750, dollar_value: 1850000, in_stock: true},
+    {name: "Pagani Huayra", horsepower: 700, dollar_value: 1300000, in_stock: false}
+  ];
+
+
+
+
+// get name of first car
+const nameOfFirstCar = _.compose(_.prop('name'), _.head)
+
+console.log(nameOfFirstCar(CARS))
+
+
+
+//get average price
+const getAverage = xs => (_.reduce(_.add, 0, xs) / xs.length)
+
+const averageDollarValue = _.compose(getAverage, _.map(_.prop('dollar_value')))
+
+console.log(averageDollarValue(CARS))
+
+
+//make car names lowercase and replace spaces with underscores
+const _underscore = _.replace(/\W+/g, '_')
+
+//const sanitizeNames = _.compose(_.map(_.toLower), _.map(_underscore), _.map(_.prop('name')))
+//get rid of nested maps
+const sanitizeNames = _.map(_.compose(_.toLower, _underscore, _.prop('name')))
+
+console.log(sanitizeNames(CARS))
+
+//get fastest car
+const prettyPrint = x => `${x.name} is the fastest 🏎!`
+
+const fastestCar = _.compose(
+    prettyPrint,
+    _.last,
+    _.sortBy(_.prop('horsepower'))
+)
+
+console.log(fastestCar(CARS))
--- curry.js
+++ curry.js
@@ -1,4 +1,5 @@
-const { curry } = require('ramda')
+import R from 'ramda'
+const _ = R
 
 const add = (x, y) => x + y
 
@@ -33,17 +34,32 @@ const isOdd = modulo(2)
 
 console.log(isOdd(12))
 
-const filter = simpleCurry((f, xs) => xs.filter(f))
+const customFilter = simpleCurry((f, xs) => xs.filter(f))
 
-const getOdds = filter(isOdd)
+const getOdds = customFilter(isOdd)
 
 console.log(getOdds([1, 2, 3, 4, 5, 6, 7]))
 
-const replace = curry((regex, replacement, str) => 
+const replace = _.curry((regex, replacement, str) => 
     str.replace(regex, replacement)
 )
 
 const replaceVowels = replace(/[AEIOU]/ig, '!')
 
 console.log(replaceVowels('Hello World'))
+
+//ex1
+
+const split = _.curry((str) => str.split(' '))
+
+console.log(split('hello world'))
+
+const listOfSentences = ['hello world', 'lorem ipsum']
+const splitMany = _.map(split)
+
+console.log(splitMany(listOfSentences))
+
+const filterQs = _.filter(_.test(/q/ig))
+
+console.log(filterQs(['quarry', 'lol', 'quake', 'lol', 'quick']))
 
